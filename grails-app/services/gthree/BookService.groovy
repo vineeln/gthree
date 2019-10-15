8e70d60e7912f8e40c196bc283bc577f44ba8578
Vineel Nalla <vineeln@gmail.com>
2019-10-15 03:57:41 -0400

    spec

--- grails-app/services/gthree/BookService.groovy
+++ grails-app/services/gthree/BookService.groovy
@@ -2,9 +2,14 @@ package gthree
 
 import grails.gorm.transactions.Transactional
 
+@groovy.util.logging.Slf4j
 @Transactional
 class BookService {
 
+    def getAuthors() {
+      log.info "In GetAuthors"
+      [ 'auth1', 'auth2' ]
+    }
     def serviceMethod() {
 
     }
--- src/integration-test/groovy/gthree/ExampleSpec.groovy
+++ src/integration-test/groovy/gthree/ExampleSpec.groovy
@@ -2,15 +2,27 @@ package gthree
 
 import grails.testing.mixin.integration.Integration
 import grails.transaction.*
-import spock.lang.Specification
+import spock.lang.*
 import org.springframework.beans.factory.annotation.*
 
 @Integration
 @Rollback
+@groovy.util.logging.Slf4j
 class ExampleSpec extends Specification {
+
+    static BookService _staticBookService;
 
     @Autowired
     BookService bookService;
+
+
+    @javax.annotation.PostConstruct
+    public void init() {
+log.info "In PostConstruct"
+println "In PostConstruct"
+//_staticBookService.test();
+      _staticBookService=bookService;
+    }
 
     def setup() {
     }
@@ -18,8 +30,21 @@ class ExampleSpec extends Specification {
     def cleanup() {
     }
 
+    @Unroll
     void "test something"() {
+        given:
+          bookService.getAuthors();
+            println "In Test"
+log.info "In Test"
+           
         expect:"fix me"
             bookService != null
+            obj != null;
+        _staticBookService != null
+
+        where:
+            //[obj] << bookService.getAuthors();
+            //[obj] << _staticBookService.getAuthors();
+            [obj] << ['abc']
     }
 }
